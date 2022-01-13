f0f6835806e537bba4080925295a4c38ab46aaca
Jacob Whitehead <jacob.whitehead@nutmeg.com>
2022-01-13 13:31:40 +0000

    Add size type

---
+++ Sources/FlowKit/SharedTypes/SizeType.swift
@@ -0,0 +1,13 @@
+//
+//  SizeType.swift
+//  
+//
+//  Created by Jacob Whitehead on 10/01/2022.
+//
+
+import SwiftUI
+
+public enum SizeType {
+    case constant(CGFloat)
+    case proportion(CGFloat)
+}
